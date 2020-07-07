ed864af95452b11c69c1f0ac9a8100854f85bc6d
Puchok-93 <paul.puchkov@yandex.ru>
2020-07-08 00:26:51 +0300

    Исправление багов

--- source/js/main.js
+++ source/js/main.js
@@ -65,3 +65,35 @@ const slideReviewsrBlock = document.querySelector(`.slider-responses__wrapper`);
       }
     });
   }
+
+  window.addEventListener('DOMContentLoaded', function() {
+    function setCursorPosition(pos, elem) {
+        elem.focus();
+        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+        else if (elem.createTextRange) {
+            var range = elem.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select()
+        }
+    }
+
+    function mask(event) {
+          var matrix = '+7(___)_______',
+              i = 0,
+              def = matrix.replace(/\D/g, ''),
+              val = this.value.replace(/\D/g, '');
+          if (def.length >= val.length) val = def;
+          this.value = matrix.replace(/./g, function(a) {
+              return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a
+          });
+          if (event.type == 'blur') {
+              if (this.value.length == 2) this.value = ""
+          } else setCursorPosition(this.value.length, this)
+      };
+      var userPhoneForm = document.querySelector('#tel');
+        userPhoneForm.addEventListener('input', mask, false);
+        userPhoneForm.addEventListener('focus', mask, false);
+        userPhoneForm.addEventListener('blur', mask, false);
+  });
